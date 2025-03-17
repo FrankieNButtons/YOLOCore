8f0022d2d88d05b088b32a7c95d29cece9c698e3
FrankieNButtons <1252077901@qq.com>
2025-03-18 00:21:03 +0800

    update the position of `hitBar`s in the video demo.

--- hitBarVideoDemowithDetector.py
+++ hitBarVideoDemowithDetector.py
@@ -4,9 +4,10 @@ from detector import Detector;
 import numpy as np;
 from typing import List, Dict, Any, Optional, Tuple;
 
-detector = Detector("./weights/yolov8s.pt");
-hitBar = hitBar(name="hitBarDemo", imgSize=(0, 320), startPoint=(300,200), endPoint=(300, 50), monitor=["person", "car", "bus"], width=10.0, maxLength=50, visualize=True);
-hitBar._monitor(["truck"]);
+detector = Detector("./weights/yolov8x.pt");
+hb1 = hitBar(name="hitBar1", imgSize=(0, 320), startPoint=(620,200), endPoint=(620, 70), monitor=["person", "car", "bus"], width=10.0, maxLength=50, visualize=True);
+hb2 = hitBar(name="hitBar2", imgSize=(0, 320), startPoint=(225,125), endPoint=(350, 70), monitor=["person", "car", "bus", "truck"], width=15.0, maxLength=50, visualize=True);
+hb1._monitor(["truck"]);
 
 video = cv2.VideoCapture("./videos/clip1.mp4");
 fps = video.get(cv2.CAP_PROP_FPS);
@@ -15,9 +16,8 @@ while True:
     if ret:
         img, detailedResult = detector.detect(frame, 
                                               addingConf=False, 
-                                              hitBars=[hitBar], 
+                                              hitBars=[hb1, hb2], 
                                               verbosity=2);
-        img, evenBetterResult = hitBar.update(img, detailedResult);
         cv2.imshow("HitBar", img);
         
         key = cv2.waitKey(int(1000 / fps));
